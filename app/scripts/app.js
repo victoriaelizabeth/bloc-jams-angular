3418f928830313ff170e3e549abed47e6471ed0c
Victoria Stubbolo <victoriastubbolo@Victorias-MacBook-Pro.local>
2017-05-19 19:47:42 -0700

    checkpoint 5 compete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,7 @@
     $stateProvider
         .state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
     })
         .state('album', {
@@ -17,7 +18,8 @@
     })
         .state('collection', {
             url: '/collection',
-             templateUrl: '/templates/collection.html'
+            controller: 'CollectionCtrl as collection',
+            templateUrl: '/templates/collection.html'
     });
 }
     
