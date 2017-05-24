555e6656983bbcdd93ad73d07d041decad666c97
Victoria Stubbolo <victoriastubbolo@Victorias-MacBook-Pro.local>
2017-05-23 22:22:50 -0700

    assignment 7 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,49 +1,73 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
-
+        
+//        @desc current song object set to null when page loads
+//        @type {Object}
         var currentSong = null;
+        
+//        @desc Buzz object audio file
+//        @type {Object}
         var currentBuzzObject = null;
-
-        /**
-        * @function setSong
-        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-        * @param {Object} song
-        */
+        
+//        @function setSong
+//        @desc Stops currently playing song and loads new audio file as currentBuzzObject
+//        @param {Object} song
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
                 currentSong.playing = null;
             }
-
+            
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
-
+            
             currentSong = song;
         };
-
+        
+//        @function playSong
+//        @desc plays song and sets song.playing to true so album.html changes play/pause icon
+//        @param {Object} song
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        
+//        @function pauseSong
+//        @desc pauses song and sets song.playing to false so album.html changes play/pause icon
+//        @param {Object} song
+        var pauseSong = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+        
+//        @function SongPlayer.play(song)
+//        @desc plays a song from the beginning if the song has not already started and continues playing the song from where it left off if not
+//        @params {Object} song
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
+                
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
-
+        
+//        @function SongPlayer.pause(song)
+//        @desc pauses a song at its current time point
+//        @params {Object} song
         SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
+            pauseSong(song);
         };
-
+        
         return SongPlayer;
     }
-
+    
     angular
         .module('blocJams')
         .factory('SongPlayer', SongPlayer);
